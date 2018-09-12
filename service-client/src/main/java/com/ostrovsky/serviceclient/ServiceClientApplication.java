8cf9a4ea0c99b787a0705696bb2a42ec006852b0
Ostrovsky <ostrovsky_zxl@163.com>
2018-09-12 20:08:18 +0800

    补充了Ribbon（负载均衡）、客户端服务集群和注册中心集群

---
+++ service-client/src/main/java/com/ostrovsky/serviceclient/ServiceClientApplication.java
@@ -0,0 +1,18 @@
+package com.ostrovsky.serviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: eurekassm
+ * @description: 客户端服务启动入口
+ * @author: Ostrovsky
+ * @create: 2018-09-12 17:41:11
+ */
+@SpringBootApplication
+public class ServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceClientApplication.class, args);
+    }
+}
---
+++ service-ribbon/src/main/java/com/ostrovsky/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,17 @@
+package com.ostrovsky.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @program: eureka-ssm
+ * @description: Ribbon服务启动入口
+ * @author: Ostrovsky
+ * @create: 2018-09-07 13:55:25
+ */
+@SpringBootApplication
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+}
